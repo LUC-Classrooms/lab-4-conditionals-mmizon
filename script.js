ad3eefb1a4b7fe2f5615f4c0bc5894a6ed3c458b
mmizon <157143739+mmizon@users.noreply.github.com>
2024-02-08 03:06:03 +0000

    Lab 4 Submitted

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function setup() {
 
 function draw() {
   background(200);
-  
+  var x = 100
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
     fill(0, 0, 255)
+    x = x+100
   }
   else { 
     // otherwise do this:
     fill(0, 100, 100)
   }
-  ellipse(width/2, height/2, 100, 100);
+  ellipse(width/2, height/2, x, x);
 }
